8c04e2d68835340d8e36b4754ec68076b84f3940
Szilard Ferencz <szilard.fer@gmail.com>
2026-05-18 17:17:33 +0200

    Updates

--- src/IdentityHub.Application/Services/GraphService.cs
+++ src/IdentityHub.Application/Services/GraphService.cs
@@ -42,7 +42,7 @@ public class GraphService : IGraphService
         {
             _logger.LogInformation("Fetching users from Graph API (top: {Top}, skip: {Skip})", top, skip);
 
-            var url = $"https://graph.microsoft.com/v1.0/users?$top={top}&$skip={skip}&$select=id,displayName,mail,userPrincipalName";
+            var url = $"https://graph.microsoft.com/v1.0/users?$top={top}&$select=id,displayName,mail,userPrincipalName";
             var users = await _graphClient.Users.WithUrl(url).GetAsync();
 
             return users?.Value?.ToList() ?? [];
